d1b932faaaae8557c45565cf5d42fc59f1376175
shervonw <w.shervonr@gmail.com>
2022-11-08 00:59:19 -0500

    create home component

---
+++ src/components/home/home.component.tsx
@@ -0,0 +1,31 @@
+import styles from "./home.module.css";
+
+const BUTTON_CONFIG = [
+  {
+    label: "Instructions",
+    type: "INSTRUCTIONS",
+  },
+  {
+    label: "Create Game",
+    type: "CREATE",
+  },
+  {
+    label: "Join",
+    type: "JOIN",
+  },
+];
+
+export const Home = ({ send }: { send: any }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Name. Place. Animal. Thing</h1>
+      <div className={styles.buttonList}>
+        {BUTTON_CONFIG.map((buttonConfig, index) => (
+          <button key={index} onClick={() => send({ type: buttonConfig.type })}>
+            {buttonConfig.label}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
