b75040b5a104878a56853c2de97e63a0fb2103bb
shervonw <w.shervonr@gmail.com>
2022-11-16 14:29:26 -0500

    revert app context

--- src/app.context.ts
+++ src/app.context.ts
@@ -1,39 +1,10 @@
-import { type } from "os";
-import { assoc, mergeDeepRight } from "ramda";
-import { createReducerContext } from "react-use";
+import { createStateContext } from "react-use";
 import { Game } from "./app.types";
 import { ALPHABET } from "./components/create-game/create-game.constants";
 
-type AppStateReducerAction = {
-  type: "SCORE" | "SET_STATE";
-  value: any;
-};
-
-interface AppContextState extends Game {
-  score: number;
-}
-
-type AppStateReducerType = (
-  state: AppContextState,
-  action: AppStateReducerAction
-) => AppContextState;
-
-const appStateReducer: AppStateReducerType = (state, action) => {
-  switch (action.type) {
-    case "SCORE":
-      return assoc("score", action.value, state);
-    case "SET_STATE":
-      return mergeDeepRight(state, action.value);
-    default:
-      state;
-  }
-};
-
-const [useAppContext, AppContextProvider] =
-  createReducerContext<AppStateReducerType>(appStateReducer, {
-    score: 0,
-    maxRounds: 5,
-    possibleAlphabet: ALPHABET,
-  });
+const [useAppContext, AppContextProvider] = createStateContext<Game>({
+  maxRounds: 5,
+  possibleAlphabet: ALPHABET,
+});
 
 export { useAppContext, AppContextProvider };
